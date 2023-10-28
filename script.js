fb10091ee8eb75845909fa07c69fa5a27dbc9d25
betelgeuse-coding <149255045+betelgeuse-coding@users.noreply.github.com>
2023-10-29 00:36:42 +0900

    버튼위치/크기 설정 ok + 사진조정

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById("button1").addEventListener("mouseover", function() {
+    this.style.opacity = 0.5; // 마우스를 올리면 불투명도를 조절하여 보이게 함
+});
+
+document.getElementById("button1").addEventListener("mouseout", function() {
+    this.style.opacity = 1; // 마우스를 내리면 다시 투명하게 함
+});
+
+document.getElementById("button2").addEventListener("mouseover", function() {
+    this.style.opacity = 0.5;
+});
+
+document.getElementById("button2").addEventListener("mouseout", function() {
+    this.style.opacity = 1;
+});
+
+document.getElementById("button3").addEventListener("mouseover", function() {
+    this.style.opacity = 0.5;
+});
+
+document.getElementById("button3").addEventListener("mouseout", function() {
+    this.style.opacity = 1;
+});
+
+document.getElementById("button4").addEventListener("mouseover", function() {
+    this.style.opacity = 0.5; // 마우스를 올리면 불투명도를 조절하여 보이게 함
+});
+
+document.getElementById("button4").addEventListener("mouseout", function() {
+    this.style.opacity = 1; // 마우스를 내리면 다시 투명하게 함
+});
