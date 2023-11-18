dfed97677e5ee0e873b1f77e135eeb991c11eea6
yoongiyoung <149480619+yoongiyoung@users.noreply.github.com>
2023-11-18 12:43:16 +0900

    Add files via upload

---
+++ map2/script.js
@@ -0,0 +1,52 @@
+//document.getElementById("button1").addEventListener("mouseover", function() {
+    this.style.opacity = 0.5; // 마우스를 올리면 반투명해집니다.
+//});
+
+//document.getElementById("button1").addEventListener("mouseout", function() {
+    this.style.opacity = 0; // 마우스를 내리면 투명해집니다.
+//});
+
+document.getElementById("button1").addEventListener("mouseover", function() {
+    this.style.opacity = 0.5; // 마우스를 올리면 반투명해집니다.
+    this.style.backgroundColor = 'rgba(99, 99, 99, 0.5)'; // 추가된 코드
+});
+
+document.getElementById("button1").addEventListener("mouseout", function() {
+    this.style.opacity = 0; // 마우스를 내리면 투명해집니다.
+    this.style.backgroundColor = 'rgba(99, 99, 99, 0)'; // 추가된 코드
+});
+
+// 위와 같은 방식으로 나머지 버튼들에도 적용하세요.
+
+
+document.getElementById("button2").addEventListener("mouseover", function() {
+    this.style.opacity = 0.5; // 마우스를 올리면 반투명해집니다.
+    this.style.backgroundColor = 'rgba(99, 99, 99, 0.5)'; // 추가된 코드
+});
+
+document.getElementById("button2").addEventListener("mouseout", function() {
+    this.style.opacity = 0; // 마우스를 내리면 투명해집니다.
+    this.style.backgroundColor = 'rgba(99, 99, 99, 0)'; // 추가된 코드
+
+
+
+});document.getElementById("button3").addEventListener("mouseover", function() {
+    this.style.opacity = 0.5; // 마우스를 올리면 반투명해집니다.
+    this.style.backgroundColor = 'rgba(99, 99, 99, 0.5)'; // 추가된 코드
+});
+
+document.getElementById("button3").addEventListener("mouseout", function() {
+    this.style.opacity = 0; // 마우스를 내리면 투명해집니다.
+    this.style.backgroundColor = 'rgba(99, 99, 99, 0)'; // 추가된 코드
+
+
+
+});document.getElementById("button4").addEventListener("mouseover", function() {
+    this.style.opacity = 0.5; // 마우스를 올리면 반투명해집니다.
+    this.style.backgroundColor = 'rgba(99, 99, 99, 0.5)'; // 추가된 코드
+});
+
+document.getElementById("button4").addEventListener("mouseout", function() {
+    this.style.opacity = 0; // 마우스를 내리면 투명해집니다.
+    this.style.backgroundColor = 'rgba(99, 99, 99, 0)'; // 추가된 코드
+});
